3aa7ea3dedafd9714fa512aad3ea9d6e2be46395
Ethan Teh <tehzl@hotmail.com>
2022-08-10 07:45:20 +0800

    dev

--- pages/lessons/form.js
+++ pages/lessons/form.js
@@ -9,18 +9,36 @@ Page({
 
   },
 
+  testPost() {
+    const data = this.getFormData();
+    console.log(data);
+  },
+
+  getFormData() {
+    const page = this;
+    console.log(page)
+    return {
+      subject: page.data.subject,
+      title: page.data.title,
+      start_date: page.data.startDate,
+      end_date: page.data.endDate,
+      description: page.data.description,
+      user_id: app.globalData.user.id
+    }
+  },
+
   createLesson() {
     const page = this;
     wx.request({
-      url: `${app.globalData.baseUrl}/create`,
+      url: `${app.globalData.baseUrl}/lessons`,
       method: 'POST',
-      data: {
-          subject: page.data.subject,
-          title: page.data.title,
-          startDate: page.data.startDate,
-          endDate: page.data.endDate,
-          description: page.data.description,
-          userId: app.globalData.user.id
+      header: app.globalData.header,
+      data: this.getFormData(),
+      success(res) {
+        console.log(res);
+        wx.switchTab({
+          url: '/pages/lessons/index',
+        })
       }
     })
   },
