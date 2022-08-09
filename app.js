d9d99d41b5d61770044982b7fae8333d3ef840e3
Ethan Teh <tehzl@hotmail.com>
2022-08-09 14:53:32 +0800

    doing frontend auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 App({
   onLaunch() {
     // 展示本地存储能力
+    const app = this;
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
@@ -9,11 +10,22 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: `${app.globalData.baseUrl}/login`,
+          method: 'post',
+          data: { code: res.code }, // pass code in request body
+          success(loginRes) {
+            console.log(loginRes) // { data: { headers: { "X-USER-TOKEN": <User Token> }, user: <User Object> }, ... }
+          }
+        })
       }
     })
   },
   globalData: {
-    userInfo: null
+    header: null,
+    user: null,
+    baseUrl: 'http://localhost:3000/api/v1',
   }
 })
