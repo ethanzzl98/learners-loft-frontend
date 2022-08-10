5c2026954c5237d4ee2e186b7d032da419d4e7b6
Illarion Kopeika <larrypearson@yandex.ru>
2022-08-10 15:19:42 +0800

    Update bookings

--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -13,17 +13,7 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-    let page = this;
-    wx.request({
-      url: `${app.globalData.baseUrl}/bookings`,
-      method: 'GET',
-      header: app.globalData.header,
-      success(res) {
-        console.log('get all bookings')
-        console.log(res.data);
-        page.setData({bookings: res.data.bookings})
-      }
-    })
+    
   },
 
   /**
@@ -37,7 +27,17 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    let page = this;
+    wx.request({
+      url: `${app.globalData.baseUrl}/bookings`,
+      method: 'GET',
+      header: app.globalData.header,
+      success(res) {
+        console.log('get all bookings')
+        console.log(res.data);
+        page.setData({bookings: res.data.bookings})
+      }
+    })
   },
 
   /**
